9a073dc54524255ddfd7104f4a8aa5bbfdb72d2a
Roman Ghazi <rog001@edu.zealand.dk>
2024-12-11 14:28:57 +0100

    Der er tilføjet test til sign up user for at se om confirm password virker. Desuden er der kørt fail test for hver test, så vi både har en, der virker og en der fejler.

--- MSFrontEndTest/SeleniumTestFrontend.cs
+++ MSFrontEndTest/SeleniumTestFrontend.cs
@@ -28,15 +28,29 @@ namespace MSFrontEndTest
         [TestMethod]
         public void CheckPageTitleSignUpUser()
         {
-
+            
             _driver.Navigate().GoToUrl("http://127.0.0.1:5500/signUpForm.html");
 
-
+            
             string pageTitleCreateUser = _driver.Title;
             
 
             Assert.AreEqual("Create User", pageTitleCreateUser);
            
+        }
+
+        [TestMethod]
+        public void CheckPageTitleSignUpUserFailTest()
+        {
+
+            _driver.Navigate().GoToUrl("http://127.0.0.1:5500/signUpForm.html");
+
+
+            string pageTitleCreateUser = _driver.Title;
+
+
+            Assert.AreEqual("Fail Test title", pageTitleCreateUser);
+
         }
 
         [TestMethod]
@@ -48,12 +62,71 @@ namespace MSFrontEndTest
 
             Assert.AreEqual("Luftkontrol", pageTitleLuftkontrol);
         }
-        
-       
 
-     
+        [TestMethod]
+        public void CheckPageTitleLuftkontrolFailTest()
+        {
+            _driver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
 
-        
+            string pageTitleLuftkontrol = _driver.Title;
+
+            Assert.AreEqual("Fail Title Test", pageTitleLuftkontrol);
+        }
+
+        [TestMethod]
+        public void SignUpFormTest()
+        {
+            _driver.Navigate().GoToUrl("http://127.0.0.1:5500/signUpForm.html");
+            
+
+            
+            IWebElement emailInput = _driver.FindElement(By.Id("email"));
+            emailInput.SendKeys("roman@example.com");
+
+           
+            IWebElement passwordInput = _driver.FindElement(By.Id("password"));
+            passwordInput.SendKeys("123456");
+            
+
+            IWebElement confirmPasswordInput = _driver.FindElement(By.Id("confirmPassword"));
+            confirmPasswordInput.SendKeys("123456");
+            Assert.AreEqual(passwordInput.GetAttribute("value"), confirmPasswordInput.GetAttribute("value"));   
+
+            
+            IWebElement signUpButton = _driver.FindElement(By.CssSelector("button.btn.btn-success"));
+            signUpButton.Click();
+
+        }
+
+        [TestMethod]
+        public void SignUpFormTestFail()
+        {
+            _driver.Navigate().GoToUrl("http://127.0.0.1:5500/signUpForm.html");
+
+
+
+            IWebElement emailInput = _driver.FindElement(By.Id("email"));
+            emailInput.SendKeys("roman@example.com");
+
+
+            IWebElement passwordInput = _driver.FindElement(By.Id("password"));
+            passwordInput.SendKeys("123456");
+
+
+            IWebElement confirmPasswordInput = _driver.FindElement(By.Id("confirmPassword"));
+            confirmPasswordInput.SendKeys("223456");
+            Assert.AreEqual(passwordInput.GetAttribute("value"), confirmPasswordInput.GetAttribute("value"));
+
+
+            IWebElement signUpButton = _driver.FindElement(By.CssSelector("button.btn.btn-success"));
+            signUpButton.Click();
+
+        }
+
+
+
+
+
 
 
 
